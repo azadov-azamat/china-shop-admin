93d3bdce9de5858ac0925f97a2d3b044b2930879
azadov-azamat <azamatazadov112@gmail.com>
2024-12-26 12:01:09 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 // import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
@@ -23,18 +23,29 @@ import ProductsController from './pages/products';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/scss/main.scss';
 import ProductCreateController from './pages/products/create';
+import { useAppSelector } from './redux/hooks.ts';
 
 function App() {
   // const [loading, setLoading] = useState<boolean>(true);
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const {auth} = useAppSelector(state => state.auth)
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 1000);
-  // }, []);
+  useEffect(() => {
+    if (auth) {
+      if (pathname === '/login') {
+        navigate('/');
+      }
+    } else {
+      if (pathname !== '/login') {
+        navigate('/login');
+      }
+    }
+  }, [auth, pathname]);
 
   const routes = [
     {
